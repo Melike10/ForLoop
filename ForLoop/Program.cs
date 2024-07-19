1a651285deea0cc15a8b48e3681ae781bd743026
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-07-19 16:41:39 +0300

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+for (int i=0; i < 10; i++)
+{
+    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+}
+//1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine(i);
+}
+//1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+for (int i = 1; i < 20; i++)
+{
+    if(i%2==0)
+    Console.WriteLine(i);
+}
+//50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+int toplam = 0;
+for (int i = 50; i < 150; i++)
+{
+    toplam += i;
+    
+}
+Console.WriteLine("50 ile 150 sayıların arasındaki toplam "+toplam);
+//1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+
+int ciftToplam = 0;
+int tekToplam = 0;
+for (int i = 1; i < 120; i++)
+{
+    if (i % 2 == 0) 
+       ciftToplam += i;
+    else 
+       tekToplam += i;      
+}
+Console.WriteLine("1 ile 120 Arasındaki Çift Sayıların Toplamı "+ciftToplam);
+Console.WriteLine("1 ile 120 Arasındaki Tek Sayıların Toplamı " + tekToplam);
+
+
+
